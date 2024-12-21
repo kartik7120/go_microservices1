9e3916add565b3315ee6185dc049ad89a4119d49
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-12-21 08:20:50 +0530

    added helper functions for accessing product data

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 )
 
@@ -47,10 +48,52 @@ func GetProducts() Products {
 	return productList
 }
 
+func AddProducts(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProducts(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
+
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
 // Using an encorder to encode the products into a json output
 // Why use an encoder instead of json.marshal ? The encoder is more efficient and can be used to encode the data directly to an io.Writer. This is useful when you want to write the data directly to a network connection or a file without having to store the entire JSON in memory.
 
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
+}
+
+// Using a decoder to decode the data send via a POST request by user
+
+func (p *Product) FromJSON(w io.Reader) error {
+	d := json.NewDecoder(w)
+	return d.Decode(p)
 }
