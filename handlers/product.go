cabd07c6c110197a32d6d5aff6ee501c0a88ffb8
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-12-21 11:11:59 +0530

    Added middleware

--- handlers/product.go
+++ handlers/product.go
@@ -1,12 +1,12 @@
 package handlers
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/kartik7120/go_microservices1/data"
 )
 
@@ -18,51 +18,55 @@ func NewProducts(l *log.Logger) *Product {
 	return &Product{l}
 }
 
-func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
-	}
+/*
+ Don't Need this code anymore
+*/
 
-	if r.Method == http.MethodPost {
-		p.addProducts(rw, r)
-		return
-	}
+// func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		p.getProducts(rw, r)
+// 		return
+// 	}
 
-	if r.Method == http.MethodPut {
-		p.l.Print("PUT request")
-		reg := regexp.MustCompile("/([0-9]+)")
+// 	if r.Method == http.MethodPost {
+// 		p.addProducts(rw, r)
+// 		return
+// 	}
 
-		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+// 	if r.Method == http.MethodPut {
+// 		p.l.Print("PUT request")
+// 		reg := regexp.MustCompile("/([0-9]+)")
 
-		if len(g) != 1 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
+// 		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
 
-		if len(g[0]) != 2 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
+// 		if len(g) != 1 {
+// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+// 			return
+// 		}
 
-		idString := g[0][1]
-		id, err := strconv.Atoi(idString)
+// 		if len(g[0]) != 2 {
+// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+// 			return
+// 		}
 
-		if err != nil {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
+// 		idString := g[0][1]
+// 		id, err := strconv.Atoi(idString)
 
-		p.updateProducts(id, rw, r)
+// 		if err != nil {
+// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+// 			return
+// 		}
 
-	}
+// 		p.updateProducts(id, rw, r)
 
-	// Catch all
+// 	}
 
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-}
+// 	// Catch all
 
-func (p *Product) getProducts(rw http.ResponseWriter, _ *http.Request) {
+// 	rw.WriteHeader(http.StatusMethodNotAllowed)
+// }
+
+func (p *Product) GetProducts(rw http.ResponseWriter, _ *http.Request) {
 	// handle the request
 	lp := data.GetProducts()
 	// d, err := json.Marshal(lp)
@@ -73,18 +77,10 @@ func (p *Product) getProducts(rw http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func (p *Product) addProducts(rw http.ResponseWriter, r *http.Request) {
+func (p *Product) AddProducts(rw http.ResponseWriter, r *http.Request) {
 	// p.l.Println("Handle POST request")
 
-	prod := data.Product{}
-
-	err := prod.FromJSON(r.Body)
-
-	if err != nil {
-		fmt.Println("Error reading product", err)
-		http.Error(rw, "Unable to marshall json", http.StatusBadRequest)
-		return
-	}
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
 	// What does this do?
 	// p.l.Printf("Prod: %#v", prod) // %#v is a Go syntax to print the struct
 
@@ -93,18 +89,18 @@ func (p *Product) addProducts(rw http.ResponseWriter, r *http.Request) {
 	data.AddProducts(&prod)
 }
 
-func (p *Product) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle PUT request")
+func (p *Product) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
 
-	prod := data.Product{}
-
-	err := prod.FromJSON(r.Body)
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
 
 	if err != nil {
-		fmt.Println("Error reading product", err)
-		http.Error(rw, "Unable to marshall json", http.StatusBadRequest)
+		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
 		return
 	}
+
+	p.l.Println("Handle PUT request")
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
 
 	e := data.UpdateProducts(id, &prod)
 
@@ -118,4 +114,24 @@ func (p *Product) updateProducts(id int, rw http.ResponseWriter, r *http.Request
 		return
 	}
 
+}
+
+type KeyProduct struct{}
+
+func (p Product) MiddleWareProductValidation(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+
+		err := prod.FromJSON(r.Body)
+
+		if err != nil {
+			http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		req := r.WithContext(ctx)
+
+		next.ServeHTTP(rw, req)
+	})
 }
